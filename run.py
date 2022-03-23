371efa6513400fe045e09c3912b075d20ecf3293
Vikramaditya Gaonkar <vikram@dataroots.io>
2022-03-23 22:30:30 +0100

    Databricks fix

--- run.py
+++ run.py
@@ -5,20 +5,15 @@ from pathlib import Path
 from src.jobs.main import jobs_main, spark_build
 from src.jobs.utils.general import EnvEnum
 from src.jobs.utils import log_utils
+file_path = "file:///databricks/python/lib/python3.8/site-packages/src/jobs/main.py"
 
 
-def main(
-    env: EnvEnum = typer.Argument(..., help="Environment for the spark-job"),
-    file_path: str = typer.Argument(
-        f"file://{Path(__file__).parent}/LICENSE", help="File which will be parsed"
-    ),
-) -> None:
+def main() -> None:
     """Execute main function for the package."""
-    with spark_build(env=env) as spark:
-        logger = log_utils.Logger(env=env, spark=spark)
-        logger.info("Spark and logger initialized")
-        jobs_main(spark, logger, file_path=file_path)
+    logger = log_utils.Logger(env=EnvEnum.dev, spark=spark)
+    logger.info("Spark and logger initialized")
+    jobs_main(spark, logger, file_path=file_path)
 
 
 if __name__ == "__main__":
-    typer.run(main)
+    main()
